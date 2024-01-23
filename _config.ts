dcb20dac53a2c4c65cb133b64b632ba733179afb
weakish <weakish@gmail.com>
2024-01-23 12:35:26 +0000

    :zzz: switch rehypeExtractExcerpt to upstream
    My pull request has been merged and released by upstream.
    It is unnecessary to use my own fork now.
    
    Also specify exact version.

--- _config.ts
+++ _config.ts
@@ -4,9 +4,9 @@ import jsx from "lume/plugins/jsx.ts";
 import pagefind from "lume/plugins/pagefind.ts";
 import remark from "lume/plugins/remark.ts";
 import resolve_urls from "lume/plugins/resolve_urls.ts";
-import rehypeStarryNight from "https://esm.sh/@microflash/rehype-starry-night";
-import rehypeExtractExcerpt from "https://esm.sh/gh/weakish/rehype-extract-excerpt@tagName";
-import rehypePicture from "https://esm.sh/rehype-picture@5";
+import rehypeStarryNight from "https://esm.sh/@microflash/rehype-starry-night@2.1.1";
+import rehypeExtractExcerpt from "https://esm.sh/rehype-extract-excerpt@0.3.1"
+import rehypePicture from "https://esm.sh/rehype-picture@5.0.0";
 import rehypeImgSize from "https://esm.sh/rehype-img-size@1.0.1";
 import sitemap from "lume/plugins/sitemap.ts";
 import imagick from "lume/plugins/imagick.ts";
