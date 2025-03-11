3d782f0d1453b7d9e70c726ad454ccd2fc6ad716
weakish <weakish@gmail.com>
2025-03-11 22:44:10 +0000

    :fire: source highlight
    Starry Night 3.6.0 failed to import under Deno:
    500 Internal Server Error from esm.sh

--- _config.ts
+++ _config.ts
@@ -6,7 +6,6 @@ import jsx from "lume/plugins/jsx.ts";
 import pagefind from "lume/plugins/pagefind.ts";
 import remark from "lume/plugins/remark.ts";
 import resolve_urls from "lume/plugins/resolve_urls.ts";
-import rehypeStarryNight from "https://esm.sh/@microflash/rehype-starry-night@2.1.1";
 import rehypeExtractExcerpt from "https://esm.sh/rehype-extract-excerpt@0.3.1"
 import rehypePicture from "https://esm.sh/rehype-picture@5.0.0";
 import rehypeImgSize from "https://esm.sh/rehype-img-size@1.0.1";
@@ -68,7 +67,7 @@ site.use(jsx());
 site.use(pagefind());
 site.use(transformImages());
 site.use(remark({
-  rehypePlugins: [rehypeStarryNight, [rehypeImgSize, { dir: "." }], [
+  rehypePlugins: [[rehypeImgSize, { dir: "." }], [
     rehypeExtractExcerpt,
     {
       name: "title",
