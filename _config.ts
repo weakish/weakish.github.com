6704da6b47861473f691e95bd10d8090612adcfd
weakish <weakish@gmail.com>
2025-03-12 16:31:05 +0000

    :new: more languages for highlight

--- _config.ts
+++ _config.ts
@@ -7,6 +7,7 @@ import pagefind from "lume/plugins/pagefind.ts";
 import remark from "lume/plugins/remark.ts";
 import resolve_urls from "lume/plugins/resolve_urls.ts";
 import rehypeStarryNight from 'https://cdn.jsdelivr.net/npm/rehype-starry-night@2/+esm'
+import { all } from 'https://cdn.jsdelivr.net/npm/@wooorm/starry-night@3.6.0/+esm'
 import rehypeExtractExcerpt from "https://esm.sh/rehype-extract-excerpt@0.3.1"
 import rehypePicture from "https://esm.sh/rehype-picture@5.0.0";
 import rehypeImgSize from "https://esm.sh/rehype-img-size@1.0.1";
@@ -69,16 +70,21 @@ site.use(jsx());
 site.use(pagefind());
 site.use(transformImages());
 site.use(remark({
-  rehypePlugins: [rehypeStarryNight, [rehypeImgSize, { dir: "." }], [
-    rehypeExtractExcerpt,
-    {
+  rehypePlugins: [[
+    rehypeStarryNight, {
+      grammars: all 
+    }], [
+    rehypeImgSize, { 
+      dir: "." 
+    }], [
+    rehypeExtractExcerpt, {
       name: "title",
       tagName: "h1",
-    },
-  ], [rehypePicture, {
-    jpg: { avif: "image/avif" },
-    png: { avif: "image/avif" },
-  }]],
+    }], [
+    rehypePicture, {
+      jpg: { avif: "image/avif" },
+      png: { avif: "image/avif" },
+    }]],
 }));
 site.use(resolve_urls());
 
